4f8739d6e5455bd4ffb1f9578bde8a24a97464e9
ultraMLGcode <ultramlg1108@gmail.com>
2017-02-02 18:18:16 -0800

    Added Main Plugin FIles

---
+++ src/main/java/me/ultramlg1108/AntiBuild/Main.java
@@ -0,0 +1,38 @@
+package me.ultramlg1108.AntiBuild;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+	
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+		getLogger().info("Enabling AntiBuild v0.0.1 by ultraMLG1108");
+	}
+	
+	@Override
+	public void onDisable() {
+		getLogger().info("Disabling AntiBuild v0.0.1 by ultraMLG1108");
+	}
+	
+	@EventHandler
+	public void antiBuild(BlockPlaceEvent event) {
+		Player player = event.getPlayer();
+		
+		if(!player.hasPermission("antibuild.build")) {
+			event.setCancelled(true);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+}

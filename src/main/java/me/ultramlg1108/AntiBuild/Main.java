c44244eed1fed7ab5350a51e4d981de58dc21a6a
ultraMLGcode <ultramlg1108@gmail.com>
2017-05-23 14:04:53 -0700

    Add BlockDestroyEvent

--- src/main/java/me/ultramlg1108/AntiBuild/Main.java
+++ src/main/java/me/ultramlg1108/AntiBuild/Main.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.BlockDestroyEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
@@ -29,7 +30,14 @@ public class Main extends JavaPlugin implements Listener {
 		}
 	}
 	
-	
+	@EventHandler
+	public void antiDestroy(BlockDestroyEvent event) {
+		Player player = event.getPlayer();
+		
+		if(!player.hasPermission("antibuild.build")) {
+			event.setCancelled(true);
+		}
+	}
 	
 	
 	

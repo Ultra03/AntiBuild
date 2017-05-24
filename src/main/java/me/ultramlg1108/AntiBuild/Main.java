35335de317ed3eb13a20fe5f05196ac3858dac6b
ultraMLGcode <ultramlg1108@gmail.com>
2017-05-23 17:44:25 -0700

    Fix BlockBreakEvent

--- src/main/java/me/ultramlg1108/AntiBuild/Main.java
+++ src/main/java/me/ultramlg1108/AntiBuild/Main.java
@@ -4,7 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
@@ -30,12 +30,13 @@ public class Main extends JavaPlugin implements Listener {
 		}
 	}
 	
-	@EventHandler
-	 public void onBlockDamage(BlockDamageEvent event) {
-                if (event.getDamageLevel()==BlockDamageLevel.BROKEN) {
-                    event.setCancelled(true);
+	public void antiBuild(BlockBreakEvent event) {
+		Player player = event.getPlayer();
+		
+		if(!player.hasPermission("antibuild.destroy")) {
+			event.setCancelled(true);
 		}
-            }
+	}
 	
 	
 	
